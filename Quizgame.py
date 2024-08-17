69c5049876021348e6e8690db827a00b322cea8c
Jeje0001 <157323284+Jeje0001@users.noreply.github.com>
2024-08-17 00:12:58 -0400

    b
    n

---
+++ Quizgame.py
@@ -0,0 +1,13 @@
+print("Welcome to the quiz game")
+
+playing=input("Do you want to play the quiz game enter yes or no?")
+def Start():
+        print("The game will start now")
+
+
+if playing == "yes":
+        Start()
+else:
+        quit()
+
+#Continue at 7:40
