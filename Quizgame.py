d56563531d6d33d4e4baa829ad0a81a11883fbec
Jeje0001 <157323284+Jeje0001@users.noreply.github.com>
2024-08-18 21:11:19 -0400

    jj
    kk

--- Quizgame.py
+++ Quizgame.py
@@ -1,6 +1,6 @@
 print("Welcome to the quiz game")
 
-playing=input("Do you want to play the quiz game enter yes or no?")
+playing=input("Do you want to play the quiz game enter yes or no?").lower()
 def Start():
         print("The game will start now")
 
@@ -10,4 +10,45 @@ if playing == "yes":
 else:
         quit()
 
-#Continue at 7:40
+score=0
+
+
+answer=input(" What does the cpu stand for ?  ").lower()
+
+if answer == "central processing unit":
+        print("You are right")
+        score+=1
+else:
+        print("You are wrong")
+
+
+answer=input(" What does the gpu stand for ?  ").lower()
+
+if answer == "graphics processing unit":
+        print("You are right")
+        score+=1
+else:
+        print("You are wrong")
+    
+
+answer=input(" What does the ram stand for ?  ").lower()
+
+if answer == "random access memory":
+        print("You are right")
+        score+=1
+else:
+        print("You are wrong")
+
+answer=input(" What does the rom stand for ?  ").lower()
+
+if answer == "read only memory":
+        print("You are right")
+        score+=1
+else:
+        print("You are wrong")
+
+
+print("You got " + str(score) + " right")
+
+percent= int(((score / 4)*100))
+print("You got " + str(percent) + " percent")
